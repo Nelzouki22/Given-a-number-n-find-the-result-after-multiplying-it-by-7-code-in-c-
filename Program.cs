c92fd3b79897cc40db0ec38797bb8f657a2fcb83
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-05-25 21:23:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            int result = n * 7;
+            Console.WriteLine("The result after multiplying {0} by 7 is: {1}", n, result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
+
